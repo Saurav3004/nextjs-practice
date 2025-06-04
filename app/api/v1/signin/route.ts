ea8b3eef34a6782d662c9bb3581cb3a807455b27
Saurav3004 <130168711+Saurav3004@users.noreply.github.com>
2025-06-04 22:21:26 +0530

    api routes

---
+++ app/api/v1/signin/route.ts
@@ -0,0 +1,12 @@
+import {  NextRequest, NextResponse } from "next/server";
+
+export default function POST(req:NextRequest){
+
+    const data = req.json()
+
+    console.log(data)
+    
+    NextResponse.json({
+        message:"Sign in successfully"
+    })
+}
